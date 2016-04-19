cf3bddf52493262a268071f8971e7211e4d2cd9b
Willyb15 <william.steen.bryant@gmail.com>
2016-04-19 16:32:12 -0400

    Init Commit
    Init Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req, res) {
+    fs.readFile('index.html', 'utf-8', function(error, data) {
+        res.writeHead(200, {
+            'content-type': "text/html"
+        });
+        res.write(data);
+        res.end();
+    });
+});
+
+// $('#chat-window').on('click', function(){
+// 	console.log('someone clicked');
+// });
+
+var io = require('socket.io').listen(server);
+var messageArray = [];
+// numUsers = 0;
+
+io.sockets.on('connect', function(socket){
+	// numUsers++;
+	// io.sockets.emit('message_to_client',{
+	// 	numUsers: numUsers
+	// });
+	
+	console.log("Someone connected to the server!!!");
+	// console.log(socket);
+	socket.on('message_to_server', function(data){
+		console.log(data);
+		var date = new Date();
+		io.sockets.emit('message_to_client',{
+			message: data.message,
+			date: date
+		});
+		messageArray.push(data.message);
+	});
+});
+server.listen(8500);
+// server.listen(2100);
+console.log("William commands to listen Port 8500. I obey. Listening on Port 8500...");
