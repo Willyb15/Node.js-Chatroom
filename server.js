74801abe1f014cb16ac6fd9c49808fb7ea6838ae
Willyb15 <william.steen.bryant@gmail.com>
2016-04-20 09:06:09 -0400

    Updating after trying Heroku Upload
    Tried to launch from Heroku but was getting errors. Going back and
    making sure everything is up working

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || '8500';
-var server = http.createServer(function(req, res) {
-    fs.readFile('index.html', 'utf-8', function(error, data) {
-        res.writeHead(200, {
-            'content-type': "text/html"
-        });
-        res.write(data);
-        res.end();
-    });
+var server = http.createServer(function(req, res){
+	fs.readFile('index.html','utf-8', function(error, data){
+		res.writeHead(200,{'content-type': 'text/html'});
+		res.write('<script>var port='+port+'</script>');
+		res.write(data);
+		res.end();		
+	});
+
 });
 
-// $('#chat-window').on('click', function(){
-// 	console.log('someone clicked');
-// });
-
 var io = require('socket.io').listen(server);
-var messageArray = [];
-// numUsers = 0;
 
+messageArray = [];
+
+numUsers = 0;
 io.sockets.on('connect', function(socket){
-	// numUsers++;
-	// io.sockets.emit('message_to_client',{
-	// 	numUsers: numUsers
-	// });
-
-	console.log("Someone connected to the server!!!");
-	// console.log(socket);
+	numUsers++;
 	socket.on('message_to_server', function(data){
-		console.log(data);
 		var date = new Date();
+		console.log(date);
 		io.sockets.emit('message_to_client',{
-			message: data.message,
+			message: data.message, 
 			date: date
 		});
 		messageArray.push(data.message);
 	});
 });
+
+
 server.listen(port);
-// server.listen(2100);
 console.log("William commands to listen Port 8500. I obey. Listening on Port 8500...");
